b1300640e142597637062a03d74a12114a409f5e
RafaelReis <rafaelisaiasreis@gmail.com>
2020-02-03 16:54:01 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -2,7 +2,9 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Optional;
 
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -12,23 +14,33 @@ import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 
-	@Autowired
-	private CategoriaRepository repo;
+  @Autowired
+  private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-	}
+  public Categoria find(Integer id) {
+    Optional<Categoria> obj = repo.findById(id);
+    return obj.orElseThrow(() -> new ObjectNotFoundException(
+        "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+  }
 
-	public Categoria insert(Categoria categoria){
-		categoria.setId(null); //O JPA entende que , como id é null, se trata de uma inserção
-		return repo.save(categoria);
-	}
+  public Categoria insert(Categoria categoria) {
+    categoria.setId(null); //O JPA entende que , como id é null, se trata de uma inserção
+    return repo.save(categoria);
+  }
 
-	public Categoria update(Categoria categoria){
-		find(categoria.getId());
-		return repo.save(categoria); // como o id não é nulo , ele atualiza o id existente
-	}
+  public Categoria update(Categoria categoria) {
+    find(categoria.getId());
+    return repo.save(categoria); // como o id não é nulo , ele atualiza o id existente
+  }
+
+  public void delete(Integer id) {
+    find(id);
+    try {
+      repo.deleteById(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+    }
+
+  }
 
 }
